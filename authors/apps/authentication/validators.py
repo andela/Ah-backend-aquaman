282db7c9a75dae78ea686a56ce18c122afe3184f
wasibani roy <roy.wasibani@andela.com>
2019-03-15 14:45:51 +0300

    fix(create user profile):  Check who is updating a Profile -Check to ensure the user updating the profile is the owner of that profile
    [Starts #164657442]

--- authors/apps/authentication/validators.py
+++ authors/apps/authentication/validators.py
@@ -8,7 +8,8 @@ from authors.apps.authentication.models import User
 
 def validate_email(email):
     check_email = User.objects.filter(email=email)
-    if not re.search(r'^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$', email):
+    email_regex = r'^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$'
+    if not re.search(email_regex, email):
         raise serializers.ValidationError("Incorrect email format please try again")
     if check_email.exists():
         raise serializers.ValidationError("This email has already been used to create a user")

b4e21041abb9ad1711c6404522aaa4366569736a
wasibani roy <roy.wasibani@andela.com>
2019-03-15 14:45:51 +0300

    fix(create user profile):  Check who is updating a Profile -Check to ensure the user updating the profile is the owner of that profile
    [Starts #164657442]

--- authors/apps/profiles/permissions.py
+++ authors/apps/profiles/permissions.py
@@ -4,8 +4,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsOwnerOrReadOnly(BasePermission):
     message = 'You are not the owner of this profile.'
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
 
         if request.method in SAFE_METHODS:
             return True
-        return obj.user == request.user
+        username = view.kwargs.get('username')
+        return request.user.username == username

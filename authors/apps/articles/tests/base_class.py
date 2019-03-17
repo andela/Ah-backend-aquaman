fc3f652c62c65a032d493de457602477aaf09729
Bagenda Deogracious <bagendadeogracious@gmail.com>
2019-03-17 18:18:34 +0300

    feat(searchArticles): search articles by author, tags, favourited
     - Ensure system users are able to search articles by author, tags, favourited
    
    [Finishes #164069234]

--- authors/apps/articles/tests/base_class.py
+++ authors/apps/articles/tests/base_class.py
@@ -11,16 +11,16 @@ class ArticlesBaseTest(test_base.BaseTest):
 
     def add_article(self):
         self.register_and_login_user()
-        response = self.client.post(self.articles_url,
+        self.client.post(self.articles_url,
                                     data=valid_article,
                                     format='json')
-        self.client.post(self.articles_url,
+        return self.client.post(self.articles_url,
                          data=valid_article,
                          format='json')
 
     def add_tagged_article(self):
         self.register_and_login_user()
-        self.client.post(self.articles_url,
+        return self.client.post(self.articles_url,
                          data=valid_article_with_tags,
                          format='json')
 

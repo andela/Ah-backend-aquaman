961c81c5577b3bd823270071f1785028d0d7a0fb
wasibani roy <roy.wasibani@andela.com>
2019-03-23 19:13:52 +0300

    fix(article): Ensure article displays comment - Ensure article displays comments related to it and there authors - Add like count to a comment on an article [Starts #164847589]

--- authors/apps/comments/serializers.py
+++ authors/apps/comments/serializers.py
@@ -7,6 +7,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     author = ProfileSerializer(read_only=True)
     commented_by = ProfileSerializer(read_only=True)
+    like_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
@@ -20,15 +21,19 @@ class CommentSerializer(serializers.ModelSerializer):
             "id",
             "first_highlited",
             "last_highlited",
-            "highlighted_text"
+            "highlighted_text",
+            "like_count"
         )
         read_only_fields = (
             "article",
             "author",
             "created_at",
             "updated_at",
-            "id"
+            "id",
+            "like_count"
         )
+    def get_like_count(self,obj):
+        return CommentLike.objects.filter(comment=obj).count()
 
 class CommentLikeSerializer(serializers.ModelSerializer):
     liked_by = ProfileSerializer(read_only=True)
